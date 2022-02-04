a9ed8e4cf14a5e294e9cb2f5ccd46d8984f1e366
root <Jayasreeb13@gmail.com>
2022-02-04 08:01:06 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  //   Write your code here.
+
+  <div class="bg">
+    <h1 class="heading">Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
